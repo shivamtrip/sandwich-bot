c15f131a425f9e4bbe53a46ca95a96078fea938a
Shivam Tripathy <shivamtrip99@gmail.com>
2023-06-26 10:09:18 -0400

    Delete build directory

--- build/sandwich_robot/CMakeFiles/sandwich_robot_genlisp.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sandwich_robot_genlisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/sandwich_robot/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/sandwich_ws/src/sandwich_robot
-# Build directory: /home/student/sandwich_ws/build/sandwich_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
